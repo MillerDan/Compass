be73a0f1f3bb63d488a5c9078b0d22a7052f77bf
Dan Miller <danm@tillercorp.com>
2019-11-28 19:35:56 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+using System.Threading;
+
+namespace Compass
+{
+    class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}
